555c0ec5ee9ca037c86067dd7cebb6072ebd8c3f
John Osezei Ikhimioya <john.osezei@yahoo.com>
2024-07-14 13:58:39 +0100

    generated page

--- app/generated/page.tsx
+++ app/generated/page.tsx
@@ -1,14 +1,14 @@
-// import React from "react";
-// import Her from "../../public/assets/mona-lisa/hero-large.jpg";
-// import Image from "next/image";
+import React from "react";
+import Her from "../../public/assets/mona-lisa/hero-large.jpg";
+import Image from "next/image";
 
-// const index = () => {
-//   return (
-//     <div>
-//       <Image src={Her} width={630} height={500} alt="her" />
-//       <h1> you see</h1>
-//     </div>
-//   );
-// };
+const index = () => {
+  return (
+    <div>
+      <Image src={Her} width={630} height={500} alt="her" />
+      <h1> you see</h1>
+    </div>
+  );
+};
 
-// export default index;
+export default index;
